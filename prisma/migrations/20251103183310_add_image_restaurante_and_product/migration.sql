b45d78dff744bfd91850fd70ceb8659966a32675
Andrew Souza <andrewvs2016@gmail.com>
2025-11-27 13:25:54 -0300

    Resolvendo bug da versao do prisma

--- prisma/migrations/20251103183310_add_image_restaurante_and_product/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "imagemURL" TEXT;
-
--- AlterTable
-ALTER TABLE "Restaurant" ADD COLUMN     "imagemURL" TEXT;
---
+++ prisma/migrations/20251127162016_init/migration.sql
@@ -0,0 +1,43 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'CLIENTE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'CLIENTE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Restaurant" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "imagemURL" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Restaurant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "restaurantId" INTEGER NOT NULL,
+    "imagemURL" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
